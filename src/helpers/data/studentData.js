bc3d78868ea87be30a14d45295cd8d114101ebcc
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-04-29 10:50:37 -0500

    stash

---
+++ src/helpers/data/studentData.js
@@ -0,0 +1,22 @@
+const students = [
+  {
+    id: 1,
+    firstName: 'Trinity',
+    lastName: 'Christiana',
+    isDead: false
+  },
+  {
+    id: 2,
+    firstName: 'Aja',
+    lastName: 'W',
+    isDead: false
+  },
+  {
+    id: 3,
+    firstName: 'Dr.',
+    lastName: 'T',
+    isDead: false
+  },
+];
+
+export default students;
