b901b45b38344cf024b15ea2f7bbe5b1bffd0edb
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-04-29 10:38:46 -0500

    getting started

--- src/App/index.js
+++ src/App/index.js
@@ -1,27 +1,9 @@
-import React, { useState } from 'react';
-import { livingStudents, followTheLight, reset } from '../helpers/data/studentData';
+import React from 'react';
 import './App.scss';
 
 function App() {
-  const [liveStudents, setLivingStudents] = useState(livingStudents());
-  const [dearlyBeloved, setDearlyBeloved] = useState([]);
-
-  const doSumthin = (cb) => {
-    const [live, dead] = cb();
-    setLivingStudents(live);
-    setDearlyBeloved(dead);
-  };
-
   return (
     <div className='App'>
-      <button onClick={() => doSumthin(followTheLight)} disabled={ liveStudents.length ? '' : 'disabled'}>{ liveStudents.length ? 'Shark Attack' : 'ALL DEAD'}</button>
-      <button onClick={() => doSumthin(reset)}>RESET</button>
-
-      <h2>Shark Tank</h2>
-      {liveStudents.map((student) => student.firstName)}
-
-      <h2>Graveyard</h2>
-      {dearlyBeloved.map((student) => student.firstName)}
     </div>
   );
 }
